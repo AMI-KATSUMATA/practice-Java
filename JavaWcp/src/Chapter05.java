1aeabc42ba9c64bd1591580b99c78107000574a1
勝股 <mjamja023@icloud.com>
2021-11-22 22:44:43 +0900

    [finish]chapter5

--- JavaWcp/src/Chapter05.java
+++ JavaWcp/src/Chapter05.java
@@ -1,4 +1,23 @@
 
 public class Chapter05 {
+	public static void main(String[] args) {
+		long num1 = 1000000000000L;
+		double num2 = 10000;
+		boolean num3 = true;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		
+		Float num4 = 0.314f;
+		Integer num5 = 5;
+		Character chara1 = 'あ';
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println(chara1);
+		
+		int[] array1 = new int[] {10, 20, 30 };
+		System.out.println(array1[2]);
+	}
+	
 
 }
