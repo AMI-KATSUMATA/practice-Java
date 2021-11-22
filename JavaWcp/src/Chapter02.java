be967f1b08fbe94b257de4cc16494ec074004d17
勝股 <mjamja023@icloud.com>
2021-11-22 22:29:12 +0900

    [finish]chapter4

---
+++ JavaWcp/src/Chapter02.java
@@ -0,0 +1,8 @@
+
+public class Chapter02 {
+    public static void main(String[] args) {
+//    	メソッドの処理を記述
+        System.out.println("Hello WEBCAMP!");
+        System.out.println("Hello Java!");
+    }
+}
---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 28;
+		String str = "勝股彩美";
+		System.out.println(str);
+
+		str = "犬は可愛い";
+		System.out.println(str);
+
+		System.out.println(LUCKY_NUMBER);
+	}
+}
